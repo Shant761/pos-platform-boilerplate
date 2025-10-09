bd6388181f448221131b98a2848f5dd59cdf83d9
Shant761 <flashbaracademia@gmail.com>
2025-10-09 15:57:55 +0400

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/pos-platform-boilerplate/'  // <- твой репозиторий
+  base: '/'   // для Vercel достаточно корня
 });
